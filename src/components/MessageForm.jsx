7c035066eeec53bf5bc7b5f246c36845ef5e1d38
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2023-10-19 16:17:59 +0530

    Storing text and imgs to Firestore

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Attachment from "./Attachment";
 
-const MessageForm = () => {
-    return <div>MessageForm</div>;
+const MessageForm = ({ handleSubmit, text, setText, setImg }) => {
+	return (
+		<form className="message_form" onSubmit={handleSubmit}>
+			<label style={{ cursor: "pointer" }} htmlFor="img">
+				<Attachment />
+			</label>
+			<input
+				type="file"
+				id="img"
+				accept="image/*"
+				style={{ display: "none" }}
+                onChange={(e) => setImg(e.target.files[0])}
+			/>
+
+			<div className="message_box">
+				<input
+					type="text"
+					name=""
+					id=""
+					placeholder="Enter message"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
+			</div>
+			<div className="send_btn">
+				<button className="btn">Send</button>
+			</div>
+		</form>
+	);
 };
 
 export default MessageForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,121 @@
 import React, { useEffect, useState } from "react";
-import { db, auth } from "../firebase";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db, auth, storage } from "../firebase";
+import {
+	collection,
+	query,
+	where,
+	onSnapshot,
+	addDoc,
+	Timestamp,
+} from "firebase/firestore";
 import User from "../components/User";
+import MessageForm from "../components/MessageForm";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 
 const Home = () => {
-    const [chat, setChat] = useState("");
-    const [users, setUsers] = useState([]);
+	const [chat, setChat] = useState("");
+	const [users, setUsers] = useState([]);
+	const [text, setText] = useState("");
+	const [img, setImg] = useState("");
 
-    useEffect(() => {
-        const usersRef = collection(db, "users");
+	const currUser = auth.currentUser;
+	const currUserUid = auth.currentUser.uid;
 
-        // query entire users collection except currently logged in user
-        const q = query(
-            usersRef,
-            where("uid", "not-in", [auth.currentUser.uid])
-        );
+	useEffect(() => {
+		const usersRef = collection(db, "users");
 
-        const unSub = onSnapshot(q, (querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                let users = [];
+		// query entire users collection except currently logged in user
+		const q = query(usersRef, where("uid", "not-in", [currUser.uid]));
 
-                querySnapshot.forEach((doc) => {
-                    users.push(doc.data());
-                });
-                setUsers(users);
-            });
-        });
-    }, []);
+		const unSub = onSnapshot(q, (querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				let users = [];
 
-    const selectUser = (user) => {
-        setChat(user);
-        // console.log(user);
-    };
+				querySnapshot.forEach((doc) => {
+					users.push(doc.data());
+				});
+				setUsers(users);
+			});
+		});
+	}, []);
 
-    // console.log(users);
+	const selectUser = (user) => {
+		setChat(user);
+		// console.log(user);
+	};
 
-    return (
-        <div className="home_container">
-            <div className="users_container">
-                {users.map((user) => (
-                    <User key={user.uid} user={user} selectUser={selectUser} />
-                ))}
-            </div>
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
+		// Adding the chat messages to the firestore db
 
-{/* ________________________________________________________________________________ */}
+		const chatUser = chat;
+		const chatUserUid = chat.uid;
 
-            <div className="messages_container">
-              {chat ? <div className="messages_user">
-                  <h3>{chat.name}</h3>
-              </div> : <h3 className="no_conv">Select a user</h3>}
-              
-            </div>
-        </div>
-    );
+		const id =
+			currUserUid > chatUserUid
+				? `${currUserUid + chatUserUid}`
+				: `${chatUserUid + currUserUid}`;
+
+		// store the sent image to the firebase
+		let url;
+		if (img) {
+			const imgRef = ref(
+				storage,
+				`images/${new Date().getTime()} - ${img.name}`
+			);
+
+			const snap = await uploadBytes(imgRef, img);
+			const imgurl = await getDownloadURL(
+				ref(storage, snap.ref.fullPath)
+			);
+			url = imgurl;
+			console.log("Image sent!");
+		}
+
+		await addDoc(collection(db, "messages", id, "chat"), {
+			text,
+			from: currUserUid,
+			to: chatUserUid,
+			createdAt: Timestamp.fromDate(new Date()),
+			media: url || "",
+		});
+
+		setText("");
+	};
+
+	// console.log(users);
+
+	return (
+		// Chats
+		<div className="home_container">
+			<div className="users_container">
+				{users.map((user) => (
+					<User key={user.uid} user={user} selectUser={selectUser} />
+				))}
+			</div>
+
+			{/* Messages */}
+			<div className="messages_container">
+				{chat ? (
+					<>
+						<div className="messages_user">
+							{/* if the user is selected */}
+							<h3>{chat.name}</h3>
+						</div>
+						<MessageForm
+							handleSubmit={handleSubmit}
+							text={text}
+							setText={setText}
+							setImg={setImg}
+						/>
+					</>
+				) : (
+					<h3 className="no_conv">Select a user</h3>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Home;
